b8df07949396198026e27ea33c7e5579e907f407
Drake Yang <drake.the.programmer@gmail.com>
2018-03-06 18:22:10 +0900

    step 4 fix 01.

--- UnitConverter/UnitConverter/main.swift
+++ UnitConverter/UnitConverter/main.swift
@@ -1,6 +1,6 @@
 //
 //  main.swift
-//  UnitConverter
+//  unitConverter
 //
 //  Created by Yoda Codd on 2018. 3. 5..
 //  Copyright © 2018년 Drake. All rights reserved.
@@ -8,47 +8,76 @@
 
 import Foundation
 // 계속 쓰이는 단위 100 을 상수로 지정
-let hund:Double = 100
+let measureHundred:Double = 100
 
-//String 값을 입력 받아서 문자열을 출력하는 함수
-func UnitConverter (inputString: String) -> String {
-    //입력값에서 문자열만 남는 변수
-    var onlyString : String = ""
-    //입력값에서 숫자 문자열만 남는 변수
-    var onlyNumberString : String = ""
-    //입력값을 배열 칸마다 배치
-    let inputStringArr = inputString
-    //배열 한글자 마다 숫자인지 알파벳인지 체크
-    for tempChar in inputStringArr {
-        // 알파벳이면 onlyString 에 추가
+//유저 입력을 받는 함수
+func recieveUserInput()->String {
+    //유저 입력을 받기 위해 입력을 요청
+    print("Please enter size : ", terminator: "")
+    //유저 입력을 받아서 userInput 에 입력
+    let userInput = readLine()!
+    return userInput
+}
+//유저가 데이터 입력
+let userInput : String = recieveUserInput()
+
+//알파벳 추출용 함수
+func extractAlphabet (inputSentence : String) -> String {
+    var inputMeasure : String = ""
+    for tempChar in inputSentence {
+        // 알파벳만 모아서 리턴
         if (tempChar >= "a" && tempChar <= "z") || (tempChar >= "A" && tempChar <= "Z") {
-            onlyString += String(tempChar)
-        }
-        // 숫자 혹은 . 이면 onlyNumberString 에 추가
-        else if(tempChar >= "0" && tempChar <= "9" || tempChar == ".")  {
-            onlyNumberString += String(tempChar)
+            inputMeasure += String(tempChar)
         }
     }
-    //숫자만 자른 숫자 string 을 더블타입으로 변환
-    let inputNum = (onlyNumberString as NSString).doubleValue
-    //문자열을 체크
-    switch onlyString{
+    return inputMeasure
+}
+// 숫자 추출용 함수
+func extractNumber (inputSentence : String) -> String {
+    var inputSize : String = ""
+    for tempChar in inputSentence {
+        // 숫자 혹은 . 만 모아서 리턴
+        if(tempChar >= "0" && tempChar <= "9" || tempChar == ".")  {
+            inputSize += String(tempChar)
+        }
+    }
+    return inputSize
+}
+//입력값에서 문자열만 남는 변수
+let inputMeasure : String = extractAlphabet(inputSentence: userInput)
+//입력값에서 숫자 문자열만 남는 변수
+let inputSize : String = extractNumber(inputSentence: userInput)
+
+//숫자만 자른 숫자 string 을 더블타입으로 변환
+let inputNumber = (inputSize as NSString).doubleValue
+
+//cm 을 m 로 바꿔서 출력
+func cmToMeter(cmNumber : Double) -> String{
+    return ("\(cmNumber/measureHundred)m")
+}
+//m 를 cm 으로 바꿔서 출렷
+func meterToCm(meterNumber : Double) -> String{
+    return ("\(Int(meterNumber*measureHundred))cm")
+}
+//m > cm , cm > m 변환 출력 함수
+func measureTransform(measure : String) -> String {
+    switch measure{
     //cm 이면 100 으로 나누고 m 을 붙여서 리턴
     case "cm" :
-        return ("\(inputNum/hund)m")
+        return (cmToMeter(cmNumber : inputNumber))
     // cm 이면 100을 곱해주고 cm을 붙여서 리턴
     case "m" :
-        return ("\(Int(inputNum*hund))cm")
+        return (meterToCm(meterNumber : inputNumber))
         
-    default : break
+    default : return("wrong measure")
     }
-    //위의 switch 구문에서 break 가 나지 않으면 fail 구문 출력
-    return ("wrong input")
 }
-//유저 입력을 받기 위해 입력을 요청
-print("Please enter size : ", terminator: "")
-//유저 입력을 받아서 userInput 에 입력
-let userInput = readLine()!
-//UnitConverter 함수에 유저 입력을 입력
-print(UnitConverter(inputString : userInput ))
+//변환 출력 함수 실행 함수
+func printResult() {
+    print(measureTransform(measure: inputMeasure))
+}
+//출력 실행
+printResult()
+
+
 
